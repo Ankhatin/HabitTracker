1bdecc33cd129bce79692b5d22fff2cbf454175e
Andrey Khatin <ankhatin@gmail.com>
2024-11-06 15:12:56 +0700

    settings.py

--- config/settings.py
+++ config/settings.py
@@ -154,9 +154,9 @@ SIMPLE_JWT = {
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:8000"]
+CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:8000"]
 
-CSRF_TRUSTED_ORIGINS = ["https://localhost:8000"]
+CSRF_TRUSTED_ORIGINS = ["https://127.0.0.1:8000"]
 
 CORS_ALLOW_ALL_ORIGINS = False
 
